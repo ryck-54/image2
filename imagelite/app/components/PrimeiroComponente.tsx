b613ff7275581f90c9f0a96ba961c986ba7f3f4a
ryckson <ryckson.matheus@estudante.ifmt.edu.br>
2026-08-10 08:42:09 -0500

    clique aqui ó

--- imagelite/app/components/PrimeiroComponente.tsx
+++ imagelite/app/components/PrimeiroComponente.tsx
@@ -1,7 +1,14 @@
+'use client'
+
 export function PrimeiroComponente() {
-  return(
+  function clique() {
+    //console.log('você clicou no botão!');
+    alert("você clicou no botãoc")
+  }
+  return (
     <div>
       <h1>Primeiro Componente</h1>
+      <button onClick={(clique)}>Clique aqui!</button>
     </div>
   );
 }
--- imagelite/app/page.tsx
+++ imagelite/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <main>
+        <h1>Welcome to Next.js!</h1>
         <PrimeiroComponente />
       </main>
     </div>
